5695f40b7701e77d75460adc69a72308ccdf44ec
llluhdx <92908756+llluhdx@users.noreply.github.com>
2021-10-21 17:32:15 +0800

    Create __init__.py

---
+++ metric_item/metric_default/os/probe/__init__.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+#######################################################################
+# Portions Copyright (c): 2021-2025, Huawei Tech. Co., Ltd.
+#
+# gs_eye is licensed under Mulan PSL v2.
+#  You can use this software according to the terms and conditions of the Mulan PSL v2.
+#  You may obtain a copy of Mulan PSL v2 at:
+#
+#           http://license.coscl.org.cn/MulanPSL2
+#
+#  THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
+#  EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+#  MERCHANTABILITY OR FIT FOR A PARTICULA# R PURPOSE.
+#  See the Mulan PSL v2 for more details.
+#######################################################################
