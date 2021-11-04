d34f9f4dc78b908c964c089eeb566ce94d31b11b
llluhdx <92908756+llluhdx@users.noreply.github.com>
2021-11-04 12:53:46 +0800

    Update cn_get_catalog_index_backward.sql

--- metric_item/metric_default/cn_get_catalog_index_backward.sql
+++ metric_item/metric_default/cn_get_catalog_index_backward.sql
@@ -1,2 +1,2 @@
-select ctid as ttid,oid,relname from pg_class where oid in(2662,2659,2679);
+select ctid,oid,relname from pg_class where oid in(2662,2659,2679);
 select ctid,attrelid,attname from pg_attribute where attrelid in(2662,2659,2679);
