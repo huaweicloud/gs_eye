bbe412e24c1b87d67355f8790da36ae3e4a88dae
llluhdx <92908756+llluhdx@users.noreply.github.com>
2021-11-04 12:55:03 +0800

    Update cn_get_db_objects.sql

--- metric_item/metric_default/cn_get_db_objects.sql
+++ metric_item/metric_default/cn_get_db_objects.sql
@@ -1,4 +1,4 @@
-select count(*) as user_count from pg_authid where oid > 16384;
-select count(*) as db_count from pg_database where oid > 16384;
-select count(*) as schema_count from pg_namespace where oid > 16384;
-select count(*) as table_count from pg_class where relkind = 'r' and oid > 16384;
+select (select count(*) from pg_authid where oid > 16384) user_count,
+       (select count(*) from pg_database where oid > 16384) db_count,
+       (select count(*) as schema_count from pg_namespace where oid > 16384) schema_count,
+       (select count(*) as table_count from pg_class where relkind = 'r' and oid > 16384) table_count;
