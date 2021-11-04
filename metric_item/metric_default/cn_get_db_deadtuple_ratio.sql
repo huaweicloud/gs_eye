64bc892348419922baeca53b790e0da275aff7ec
llluhdx <92908756+llluhdx@users.noreply.github.com>
2021-11-04 12:54:27 +0800

    Update cn_get_db_deadtuple_ratio.sql

--- metric_item/metric_default/cn_get_db_deadtuple_ratio.sql
+++ metric_item/metric_default/cn_get_db_deadtuple_ratio.sql
@@ -1,6 +1,1 @@
-select pg_stat_get_live_tuples('pg_class'::regclass) as live, pg_stat_get_dead_tuples('pg_class'::regclass) as dead, dead/(dead+live) as pg_class_dead_ratio;
-select pg_stat_get_live_tuples('pg_attribute'::regclass) as live, pg_stat_get_dead_tuples('pg_attribute'::regclass) as dead, dead/(dead+live) as pg_attribute_dead_ratio;
-select pg_stat_get_live_tuples('pg_proc'::regclass) as live, pg_stat_get_dead_tuples('pg_proc'::regclass) as dead, dead/(dead+live) as pg_proc_dead_ratio;
-select pg_stat_get_live_tuples('pg_depend'::regclass) as live, pg_stat_get_dead_tuples('pg_depend'::regclass) as dead, dead/(dead+live) as pg_depend_dead_ratio;
-select pg_stat_get_live_tuples('pg_partition'::regclass) as live, pg_stat_get_dead_tuples('pg_partition'::regclass) as dead, dead/(dead+live) as pg_partition_dead_ratio;
-select pg_stat_get_live_tuples('pg_statistic'::regclass) as live, pg_stat_get_dead_tuples('pg_statistic'::regclass) as dead, dead/(dead+live) as pg_statistic_dead_ratio;
+select relname,pg_stat_get_live_tuples(oid) as live, pg_stat_get_dead_tuples(oid) as dead from pg_class where oid in (1249,1255,1259,2608,2619,9016);
