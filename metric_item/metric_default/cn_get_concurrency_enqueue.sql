2476bdf72c0d2145cb750e934c5b9c0a555303ed
K2lord <56468683+K2lord@users.noreply.github.com>
2021-11-08 11:10:42 +0800

    Add files via upload

--- metric_item/metric_default/cn_get_concurrency_enqueue.sql
+++ metric_item/metric_default/cn_get_concurrency_enqueue.sql
@@ -4,8 +4,6 @@ SELECT usename
  ,enqueue
  ,count(*)
 FROM pgxc_stat_activity
-WHERE STATE = 'idle'
- AND usename <> 'omm'
- AND usename <> 'Ruby'
+WHERE STATE <> 'idle'
 GROUP BY 1,2,3,4
 ORDER BY 5 desc;
