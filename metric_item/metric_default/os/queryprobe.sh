4e214038e2ef74c3bf9890c36d7270e3eb5ea375
llluhdx <92908756+llluhdx@users.noreply.github.com>
2021-11-11 15:38:34 +0800

    Update queryprobe.sh

--- metric_item/metric_default/os/queryprobe.sh
+++ metric_item/metric_default/os/queryprobe.sh
@@ -14,22 +14,7 @@
 #  See the Mulan PSL v2 for more details.
 #######################################################################
 
-echo "
-    set_time                float,
-    start_xact_time         float,
-    create_time             float,
-    insert_time             float,
-    gather_time             float,
-    redistribute_min_time   float,
-    redistribute_max_time   float,
-    scan_tB_min_time        float,
-    scan_tB_max_time        float,
-    scan_tA_min_time        float,
-    scan_tA_max_time        float,
-    analyze_total_time      float,
-    drop_time               float,
-    commit_time             float,
-    total_probe_time        float
-"
-echo "python \${METRIC_ITEM}os/probe/query_probe.py"
-python ${METRIC_ITEM}os/probe/query_probe.py
+export METRIC_BASE=`pwd`/../../../
+export METRIC_ITEM=${METRIC_BASE}/metric_item
+export METRIC_LIB=${METRIC_BASE}/bin
+python ${METRIC_ITEM}/metric_default/os/probe/query_probe.py
